7e8a894a1ce0013fc406d14608c4e1e4bcecd766
MattChtn Development <jbraultmattheo36500@gmail.com>
2025-09-30 23:08:55 +0200

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'MattChtn'
+description 'Script that send frenquently a notification to everyone on your FiveM server | Script qui envoie fréquemment une notification à tout le monde sur ton serveur Fivem'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
+
+shared_script 'config.lua'
